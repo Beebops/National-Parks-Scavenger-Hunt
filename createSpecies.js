259d3a5fdf11c8ced9bc8c64ec202c19538e1881
Beth Carretta <bethcarretta@gmail.com>
2023-12-27 09:50:02 -0500

    started writing class to extract species data from various APIs

---
+++ createSpecies.js
@@ -0,0 +1,43 @@
+require('dotenv').config({ path: './.env.development' })
+const axios = require('axios')
+
+GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+BASE_URL = `https://kgsearch.googleapis.com/v1/entities:search?indent=true&limit=1&key=`
+IRM_BASE_URL = 'https://irmaservices.nps.gov/v3/rest/npspecies/fulllist/'
+
+async function getSpeciesGoogleKGData(species) {
+  // return a partial species obj { description, imageUrl, wikiLink}
+
+  try {
+    const response = await axios.get(
+      `${BASE_URL}${GOOGLE_API_KEY}&query=${species}`
+    )
+    const {
+      image: { url: imageUrl },
+      detailedDescription: { articleBody: description, url: wikiLink },
+    } = response.data.itemListElement[0].result
+
+    return { imageUrl, description, wikiLink }
+  } catch (e) {
+    console.error(e)
+    return {}
+  }
+}
+
+async function getSpeciesIRMAData(parkCode, category) {
+  try {
+    const response = await axios.get(`${IRM_BASE_URL}/${parkCode}/${category}`)
+    const speciesData = response.data
+    const allSpecies = speciesData.map((species) => ({
+      scientificName: species.ScientificName,
+      commonNames: species.CommonNames,
+    }))
+    console.log(allSpecies)
+    return allSpecies
+  } catch (e) {
+    console.error(e)
+    return {}
+  }
+}
+getSpeciesIRMAData('zion', 'mammals')
+//getSpeciesGoogleKGData('bald eagle')
