1ff3e685106694d3d5cf01d00f9808737dbda024
Beth Carretta <bethcarretta@gmail.com>
2023-12-29 11:53:53 -0500

    added routes and methods for user creation, login, updating, deleting

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,12 +2,58 @@ const express = require('express')
 const router = express.Router()
 
 module.exports = (userService) => {
-  router.get('/', async (req, res) => {
+  // Get all users
+  router.get('/', async (req, res, next) => {
     try {
       const users = await userService.getUsers()
-      res.send(users)
+      res.json(users)
     } catch (err) {
-      return next(err)
+      next(err)
+    }
+  })
+
+  // Get a single user by user_id
+  router.get('/:userId', async (req, res, next) => {
+    {
+      try {
+        const user = await userService.getUserById(req.params.userId)
+        res.json(user)
+      } catch (err) {
+        next(err)
+      }
+    }
+  })
+
+  // Create a new user
+  router.post('/', async (req, res, next) => {
+    try {
+      const newUser = await userService.createUser(req.body)
+      res.status(201).json(newUser)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  // Update user
+  router.put('/:userId', async (req, res, next) => {
+    try {
+      const updatedUser = await userService.updateUser(
+        req.params.userId,
+        req.body
+      )
+      res.json(updatedUser)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  // Delete user
+  router.delete('/:userId', async (req, res, next) => {
+    try {
+      await userService.deleteUser(req.params.userId)
+      res.status(204).send()
+    } catch (err) {
+      next(err)
     }
   })
 
--- services/UserService.js
+++ services/UserService.js
@@ -1,16 +1,32 @@
 const pool = require('../db')
+const bcrypt = require('bcrypt')
+const ExpressError = require('../expressError')
 
 class UserService {
   async getUsers() {
-    const result = await pool.query(`SELECT * FROM users`)
+    const result = await pool.query(`SELECT user_id, username FROM users`)
     return result.rows
   }
 
-  async createUser(username, password) {
+  async getUserById(userId) {
+    const result = await pool.query(
+      `SELECT user_id, username FROM users WHERE user_id = $1`,
+      [userId]
+    )
+    if (result.rows.length === 0) throw new ExpressError('User not found', 404)
+    return result.rows[0]
+  }
+
+  async createUser({ username, email, password }) {
+    if (!username || !email || !password)
+      throw new ExpressError('Username, email, and password are required', 400)
+
+    console.log('Creating user with:', { username, email, password })
+
     const hashedPassword = await bcrypt.hash(password, 10)
     const result = await pool.query(
-      `INSERT into users (username, password) VALUES ($1, $2) RETURNING *`,
-      [username, hashedPassword]
+      `INSERT into users (username, email, user_password) VALUES ($1, $2, $3) RETURNING *`,
+      [username, email, hashedPassword]
     )
     return result.rows[0]
   }
@@ -29,9 +45,33 @@ class UserService {
         )
         return { user, token }
       }
-      throw new Error('Invalid credentials')
+      throw new ExpressError('Invalid credentials', 401)
     }
-    throw new Error('User not found')
+    throw new ExpressError('User not found', 404)
+  }
+
+  async updateUser(userId, updateData) {
+    const entries = Object.entries(updateData)
+    const updates = entries
+      .map(([key, value], index) => `${key} = $${index + 1}`)
+      .join(', ')
+    const values = entries.map(([key, value]) => value)
+
+    if (!updates) throw new ExpressError('No update data provided', 400)
+
+    try {
+      const result = await pool.query(
+        `UPDATE users SET ${updates} WHERE user_id = $${
+          entries.length + 1
+        } RETURNING *`,
+        [...values, userId]
+      )
+      if (result.rows.length === 0)
+        throw new ExpressError('User not found', 404)
+      return result.rows[0]
+    } catch (err) {
+      throw new ExpressError('Error updating user', 500)
+    }
   }
 
   async deleteUser(userId) {
